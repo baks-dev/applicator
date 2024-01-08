70564e0b394c7d637d6e85b2e2d8efc19dfb8d7a
kingstar <admin@baks.dev>
2024-01-08 16:27:14 +0300

    Baks Development

--- homepage.js
+++ homepage.js
@@ -1,4 +1,64 @@
 
-function applicator(param) {
+
+async function applicator(param) {
+
+    var table = [];
+
+    let response = await fetch('https://tmdb.cub.red/?sort=now_playing&page=1');
+
+    let json =  await response.json(); // прочитать тело ответа как текст
+
+    //console.log(json.results);
+
+    json.results.forEach(function (item, index) {
+
+        const date = new Date(item.release_date);
+
+        let itm = {};
+        itm.id = item.id
+        itm.title = item.title
+        itm.image = item.poster_path
+        itm.year = date.getFullYear();
+        itm.rate = item.vote_average
+
+        table[index] = itm;
+
+
+    });
+
+
+    //console.log(table);
+
+    return table;
+}
+
+function applicatorOLD(param) {
+
+
+
+    let response = fetch('https://tmdb.cub.red/?sort=now_playing&page=1');
+
+    if (response.ok) {
+
+        //let json = response.json();
+
+        const obj = JSON.parse(response.json());
+
+        obj.results.forEach(function (item, i, arr) {
+
+            item.id,
+                item.name
+
+            //item.checked = $this.dataset.id === item.dataset.id;
+        });
+
+
+    } else {
+        alert("Ошибка HTTP: " + response.status);
+    }
+
+
     return param;
 }
+
+
