c9657e33e4fa906ec3820282c28b60a9765d0378
kingstar <admin@baks.dev>
2024-01-08 18:35:19 +0300

    Baks Development

--- homepage.js
+++ homepage.js
@@ -6,20 +6,25 @@ async function applicator(param) {
 
     let response = await fetch('https://tmdb.cub.red/?sort=now_playing&page=1');
 
-    let json =  await response.json();
+    let json = await response.json();
 
     //console.log(json.results);
 
     json.results.forEach(function (item, index) {
 
-        const date = new Date(item.release_date);
+        //console.log(item);
+        // console.log(item.title);
+        // console.log(item.name);
+
+        const date = new Date(item.release_date ? item.release_date : item.first_air_date);
 
         let itm = {};
         itm.id = item.id
-        itm.title = item.title
-        itm.image = 'https://imagetmdb.cub.red/t/p/w300'+item.poster_path
+        itm.title = item.title ? item.title : item.name;
+        itm.image = 'https://imagetmdb.cub.red/t/p/w300' + item.poster_path
         itm.year = date.getFullYear();
-        itm.rate = item.vote_average
+        itm.rate = item.vote_average.toFixed(1);
+
 
         table[index] = itm;
 
